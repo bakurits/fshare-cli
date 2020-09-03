27e4ea20a596009e1e4dfc4014a4da7e6b2bef71
Giorgi <gbagh16@freeuni.edu.ge>
2020-09-04 01:26:41 +0400

    add createDir, createFile methods in package drive, add main function for creation a folder. (tu rame ki daareverteb amas da me axal branchs vizam tusashineleba ar vqeni :DDD)

--- pkg/drive/list.go
+++ pkg/drive/list.go
@@ -6,7 +6,7 @@ import (
 
 // List returns list of files contained in drive
 func (s Service) List() []*drive.File {
-	r, err := s.drive.Files.List().OrderBy("createdTime desc").PageSize(20).
+	r, err := s.Drive.Files.List().OrderBy("createdTime desc").PageSize(20).
 		Fields("nextPageToken, files(id, name)").Do()
 	if err != nil {
 		return []*drive.File{}
