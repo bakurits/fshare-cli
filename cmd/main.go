591a6a08cc06b2d704a74f5a7e969c1bfb4012ea
Giorgi <gbagh16@freeuni.edu.ge>
2020-09-04 19:16:41 +0400

    changes in main.go. uploaded my idedntical card photo in google drive.

--- cmd/main.go
+++ cmd/main.go
@@ -2,38 +2,43 @@ package main
 
 import (
 	"fmt"
-	"github.com/bakurits/fileshare/pkg/auth"
 	"github.com/bakurits/fileshare/pkg/drive"
+	"mime"
 	"os"
+	"path/filepath"
 )
+
+func getFilePathMimeType(filePath string) string {
+	ext := filepath.Ext(filePath)
+	tp := mime.TypeByExtension(ext)
+	return tp
+}
+
+func uploadFile(filepath string, service drive.Service) {
+	f, err := os.Open(filepath)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(fmt.Sprintf("cannot open file: %v", err))
+	}
+
+	defer f.Close()
+
+	tp := getFilePathMimeType(filepath)
+
+	file, err := drive.CreateFile(service.Drive, filepath, tp, f, "root")
+	fmt.Println(file)
+
+	return
+}
 
 func main() {
 	creditials, err := os.Getwd()
 	creditials = creditials + "/credentials"
 	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(creditials)
-
-	client, err := auth.GetHTTPClient(creditials)
-
-	if err != nil {
-		fmt.Println(err)
+		fmt.Sprintf("cannot open file: %v", err)
 	}
 
-	service, err := drive.NewService(client)
-
-	if err != nil {
-		panic(fmt.Sprintf("Could not create service: %v\n", err))
-	}
-
-	dir, err := drive.CreateDir(service.Drive, "My Folder", "root")
-
-	if err != nil {
-		panic(fmt.Sprintf("Could not create dir: %v\n", err))
-	}
-
-	fmt.Println("kargadaa yvelaferi")
-
-	fmt.Println(dir)
+	service, err := drive.Authorize(creditials)
+	uploadFile("C:\\Users\\Giorgi\\Downloads\\IMG_20200811_164530.jpg", service)
 }
