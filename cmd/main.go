bd7fc62bb1e4b06ef2e98f9812fab4475699b83b
Giorgi <gbagh16@freeuni.edu.ge>
2020-09-21 15:22:51 +0400

    add terminal package for command classification. now you can enter multiple query without program ending. you can just type exit to exit from terminal.

--- cmd/main.go
+++ cmd/main.go
@@ -1,59 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/bakurits/fileshare/pkg/terminal"
 	"log"
-	"mime"
 	"os"
-	"path/filepath"
-
-	"github.com/bakurits/fileshare/pkg/drive"
 )
 
-func getFilePathMimeType(filePath string) string {
-	ext := filepath.Ext(filePath)
-	tp := mime.TypeByExtension(ext)
-	return tp
-}
-
-func uploadFile(filepath string, s *drive.Service) {
-	f, err := os.Open(filepath)
-
-	if err != nil {
-		fmt.Println(err)
-		panic(fmt.Sprintf("cannot open file: %v", err))
-	}
-
-	defer f.Close()
-
-	tp := getFilePathMimeType(filepath)
-
-	file, err := s.CreateFile(filepath, tp, f, "root")
-	fmt.Println(file)
-
-	return
-}
-
 func main() {
-	creditials, err := os.Getwd()
-	creditials = creditials + "/credentials"
-	if err != nil {
-		log.Fatalf("cannot open file: %v", err)
+	//creditials, err := os.Getwd()
+
+	terminal := terminal.GetInstance()
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("$ ")
+		cmdString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+		}
+		err = terminal.Execute(cmdString)
+		if err != nil {
+			log.Println(err)
+		}
 	}
-
-	service, err := drive.Authorize(creditials)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	fmt.Printf("Enter The File Path \n")
-	var filePath string
-	if _, err := fmt.Scan(&filePath); err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	fmt.Println(filePath)
-
-	uploadFile(filePath, service)
 
 }
