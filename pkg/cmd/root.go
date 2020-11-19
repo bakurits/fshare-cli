4a2cf0053d605fed1bf86b2545307de3fd7624e7
Giorgi <gbagh16@freeuni.edu.ge>
2020-11-19 21:10:06 +0400

    fix : config is now loadded, but we have small bugs. will be fixed next commits.

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -9,9 +9,6 @@ import (
 	"github.com/spf13/cobra"
 	"log"
 	"os"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -43,19 +40,17 @@ func Execute() {
 
 func init() {
 
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.fileshare.json)")
+	initConfig()
 
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	rootCmd.AddCommand(drivemanager.AuthorizeCommand{Host: conf.Host, TokenPath: conf.TokenPath}.New())
-
 	rootCmd.AddCommand(drivemanager.UploadFileCommand{AuthClient: authClient}.New())
 	rootCmd.AddCommand(drivemanager.CreateDirCommand{AuthClient: authClient}.New())
 	rootCmd.AddCommand(drivemanager.ListCommand{AuthClient: authClient}.New())
 	rootCmd.AddCommand(drivemanager.DownloadCommand{AuthClient: authClient}.New())
 	rootCmd.AddCommand(mail.SendAttachmentCommand{AuthClient: authClient}.New())
+
 }
 
 func readConfig() {
@@ -63,7 +58,6 @@ func readConfig() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	cred, err := cfg.LoadGoogleCredentials(conf.GoogleCredentialsPath, cfg.CredentialTypeDesktop)
 	if err != nil {
 		log.Fatal(err)
@@ -71,38 +65,19 @@ func readConfig() {
 	conf.GoogleCredentials = cred
 }
 
-func getAuthClient() {
+func getAuthClient() error {
 	var err error
 	authClient, err = auth.
 		GetConfig(conf.GoogleCredentials.ClientID, conf.GoogleCredentials.ClientSecret, "http://localhost").
 		ClientFromTokenFile(conf.TokenPath)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	return nil
 }
 
 // initConfig : initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".fileshare" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".fileshare")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	readConfig()
+	_ = getAuthClient()
 }
